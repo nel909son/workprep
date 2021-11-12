df69b223d9eefb6a66206f5c17e3d09610312e37
Nelson <nelsonoffley@gmail.com>
2021-11-12 12:05:42 -0800

    still brain dead but fixed a typo

--- rmDupSortArr.py
+++ rmDupSortArr.py
@@ -25,8 +25,9 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         ##Input: nums = [1,2,3,4,5,6,7], k = 3
         ##Output: [5,6,7,1,2,3,4]
+        pass
 
-        
+
         
         
     
